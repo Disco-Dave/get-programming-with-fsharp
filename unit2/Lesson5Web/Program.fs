0cbe85b13fc2c68493282454112c2d9c73b9a88f
Disco <Disco@DESKTOP-F01FS8D>
2020-05-31 16:19:46 -0700

    Did the lesson 5 web browser win form example

---
+++ unit2/Lesson5Web/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.Net
+open System.Windows.Forms
+
+let getHtml (uri: Uri) =
+    use webClient = new WebClient()
+    webClient.DownloadString uri
+
+let createBrowser documentText =
+    new WebBrowser(ScriptErrorsSuppressed = true,
+                   Dock = DockStyle.Fill,
+                   DocumentText = documentText)
+
+let createForm title uri  =
+    let browser = createBrowser (getHtml uri)
+    let form = new Form(Text = title)
+    form.Controls.Add browser
+    form
+
+[<EntryPoint; STAThread>]
+let main _ =
+    Application.SetHighDpiMode(HighDpiMode.SystemAware) |> ignore
+    Application.EnableVisualStyles()
+    Application.SetCompatibleTextRenderingDefault(false)
+
+    use form = createForm "FSharp Org" (Uri "http://fsharp.org")
+    Application.Run(form)
+
+    0 // return an integer exit code
